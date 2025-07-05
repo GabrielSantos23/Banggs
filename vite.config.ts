33827bb802e61c2046e5c606fdc0526cea18f72a
gabriel <gabriel.gs605@gmail.com>
2025-07-05 19:23:28 -0300

    feat: corrected build error

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,20 @@ export default defineConfig({
   plugins: [
     VitePWA({
       registerType: "autoUpdate",
+      workbox: {
+        maximumFileSizeToCacheInBytes: 4 * 1024 * 1024,
+      },
     }),
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("bangs.json")) {
+            return "bangs";
+          }
+        },
+      },
+    },
+  },
 });
